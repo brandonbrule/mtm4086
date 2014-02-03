05638f69be69170c4c806b6998ba5813fc3e8ae5
brandon.brule@bv02.com <brandon.brule@bv02.com>
2014-02-03 12:13:57 -0500

    dynamic iframe

--- week05/animation-mixin/js/index.js
+++ week05/animation-mixin/js/index.js
@@ -2,4 +2,5 @@
 // Sidebar
 $( ".banner" ).click(function() {
   $(".interior-video-wrapper").toggleClass('animate');
+  $(".banner iframe").attr('src','http://player.vimeo.com/video/25708134?title=0&amp;byline=0&amp;portrait=0');
 });
